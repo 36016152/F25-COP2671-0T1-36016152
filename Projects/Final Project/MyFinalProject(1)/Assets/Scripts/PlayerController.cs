3c0882607bee9eeaa6475e94bb2644e3a4b0e8b9
Mira Melendez <36016152@sfcollege.edu>
2025-12-02 21:35:03 -0500

    Final Project Project Setup

---
+++ Projects/Final Project/MyFinalProject(1)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+         
+    }
+
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector2.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector2.up * verticalInput * Time.deltaTime * speed);
+    }
+}
